09553bca4f607495c8d00e44cf75cf25564007e3
projectwakii <projectwakii@gmail.com>
2015-06-27 12:38:57 +1000

    Initial Commit

---
+++ BodyCompare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BodyCompare
+//
+//  Created by David Liu on 27/06/2015.
+//  Copyright © 2015 David Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
