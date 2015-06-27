04e240c44859f1a3fbe5bc2c6b17c2de5d6836d4
projectwakii <projectwakii@gmail.com>
2015-06-27 14:11:34 +1000

    Implementing a UIImagePickerController with an action sheet pop-up.

--- BodyCompare/ViewController.swift
+++ BodyCompare/ViewController.swift
@@ -8,18 +8,99 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var idealImage: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        idealImage.image = UIImage(named: "idealBody.jpeg")
+        imagePicker.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var imageButtonImage: UIImageView!
+    @IBAction func imageButtonDidPress(sender: AnyObject) {
+        print("pressed")
+        
+        
+        //show the action sheet (i.e. the little pop-up box from the bottom that allows you to choose whether you want to pick a photo from the photo library or from your camera 
+        let optionMenu = UIAlertController(title: nil, message: "Where would you like the image from?", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let photoLibraryOption = UIAlertAction(title: "Photo Library", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction!) -> Void in
+            print("from library")
+            //shows the photo library
+            self.imagePicker.allowsEditing = true
+            self.imagePicker.sourceType = .PhotoLibrary
+            self.imagePicker.modalPresentationStyle = .Popover
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
+        })
+        let cameraOption = UIAlertAction(title: "Take a photo", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction!) -> Void in
+            print("take a photo")
+            //shows the camera
+            self.imagePicker.allowsEditing = true
+            self.imagePicker.sourceType = .Camera
+            self.imagePicker.modalPresentationStyle = .Popover
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
+
+        })
+        let cancelOption = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: {
+            (alert: UIAlertAction!) -> Void in
+            print("Cancel")
+            self.dismissViewControllerAnimated(true, completion: nil)
+        })
+        
+        //Adding the actions to the action sheet. Camera will only show up as an option if the camera is available in the first place.
+        optionMenu.addAction(photoLibraryOption)
+        optionMenu.addAction(cancelOption)
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) == true {
+            optionMenu.addAction(cameraOption)} else {
+            print ("I don't have a camera.")
+        }
+
+        
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+        
+        /*
+        */
+    }
+    
+    // MARK: - Image Picker Delegates
+    //The UIImagePickerController is a view controller that gets presented modally. When we select or cancel the picker, it runs the delegate, where we handle the case and dismiss the modal. 
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        print("finished picking image")
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        //handle media here i.e. do stuff with photo
+        
+        print("imagePickerController called")
+        
+            let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            imageButtonImage.image = chosenImage
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        //what happens when you cancel
+        //which, in our case, is just to get rid of the photo picker which pops up
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 
 }
+
 
